9a920a8523ff0b6538618579b74831f0cf4cc168
double J <valorjj@gmail.com>
2021-11-15 14:18:00 +0900

    jj branch, why conflicts?

--- fx_sample/src/application/Main.java
+++ fx_sample/src/application/Main.java
@@ -10,7 +10,6 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Font.loadFont(getClass().getResourceAsStream("/fonts/NanumBarunGothicBold.ttf"), 10); //폰트 불러오기
 			Parent parent = FXMLLoader.load(getClass().getResource("/fxml/login_page.fxml"));
 			Scene scene = new Scene(parent);
 
